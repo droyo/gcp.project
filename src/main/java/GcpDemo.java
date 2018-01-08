64474d6638cbb5b04e683d1d26ebe49a50d08cb6
David Arroyo <arroyod@google.com>
2018-01-08 10:41:21 -0500

    Example fetch tests from GCS instead of maven central

--- src/main/java/GcpDemo.java
+++ src/main/java/GcpDemo.java
@@ -1,15 +1,12 @@
 package com.github.droyo.gcpdemo;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class GcpDemo {
+  private final static Logger logger = LogManager.getLogger("GcpDemo");
+
   public static void main(String[] args) {
-    try {
-      GoogleCredential credential = GoogleCredential.getApplicationDefault();
-      System.out.println("Service account: " + credential.getServiceAccountId());
-    } catch (Throwable t) {
-      t.printStackTrace();
-      System.exit(1);
-    }
+    logger.error("test");
   }
 }
